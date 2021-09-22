4cc2f14067861ea5e68bdde95f518f1703dddf65
Nicolae <lupu.nicolae99@gmail.com>
2021-09-22 14:06:16 +0300

    "implemented UI"

--- ToDoList/App/AppDelegate.swift
+++ ToDoList/App/AppDelegate.swift
@@ -9,34 +9,36 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-var window: UIWindow?
-var navController: UINavigationController?
-var ViewController: ViewController?
-
+    
+    var window: UIWindow?
+    var navController: UINavigationController?
+    var viewController: ViewController?
+    var createViewController: CreateViewController?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame:UIScreen.main.bounds)
+        self.viewController = ViewController(nibName: "ViewController", bundle: nil)
+        self.createViewController = CreateViewController(nibName: "CreateViewController", bundle: nil)
+        navController = UINavigationController(rootViewController:  createViewController!)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
     
-    window = UIWindow(frame:UIScreen.main.bounds)
-    ViewController = ToDoList.ViewController(nibName: "ViewController", bundle: nil)
-    navController = UINavigationController(rootViewController:  ViewController!)
-    window?.rootViewController = navController
-    window?.makeKeyAndVisible()
+    func applicationWillResignActive(_ application: UIApplication) {
+    }
     
-    return true
-}
-func applicationWillResignActive(_ application: UIApplication) {
-
-}
-func applicationDidEnterBackground(_ application: UIApplication) {
-
-}
-func applicationWillEnterForeground(_ application: UIApplication) {
+    func applicationDidEnterBackground(_ application: UIApplication) {
+    }
     
-}
-func applicationDidBecomeActive(_ application: UIApplication) {
+    func applicationWillEnterForeground(_ application: UIApplication) {
+    }
     
-}
-func applicationWillTerminate(_ application: UIApplication) {
+    func applicationDidBecomeActive(_ application: UIApplication) {
+    }
     
-}
+    func applicationWillTerminate(_ application: UIApplication) {
+    }
 }
